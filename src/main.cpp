1a5a944e32399db631e1cd75ef30f75e1f86aed6
Robin Touche <NA>
2018-11-28 17:16:29 +0100

    First test of sprite and translation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(500,500), "BLAAAAH");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1000,1000), "BLAAAAH");
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("box.png")) {
+        exit(1);
+    }
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -13,8 +19,19 @@ int main() {
             }
         }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            sprite.move(-0.1, 0);
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            sprite.move(0.1, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            sprite.move(0, -0.1);
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            sprite.move(0, 0.1);
+        }
+
         window.clear();
-        window.draw(shape);
+        window.draw(sprite);
         window.display();
     }
 }
