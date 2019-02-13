8254b685e10e6568b9291c46b4e83d3eaffe31f0
Robin Touche <robin.touche@volvocars.com>
2019-02-13 10:13:09 +0100

    Initial state graph generator commit

--- src/state_graph/main.cpp
+++ src/state_graph/main.cpp
@@ -1,3 +1,21 @@
-int main() {
+#include <iostream>
+#include <fstream>
 
+#define GRAPH_NAME "state"
+
+int main() {
+    std::ofstream of("state.dot");
+
+    if (!of.good()) {
+        std::cout << "Failed to open ouput file" << std::endl;
+        exit(1);
+    }
+
+    std::string test("A1 -> A2");
+
+    of << "digraph " << GRAPH_NAME << " {" << std::endl;
+
+    of << test << ";" << std::endl;
+
+    of << "}" << std::endl;
 }
