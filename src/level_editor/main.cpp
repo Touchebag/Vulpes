e54f7337db7da0436fa82051be05c9a6293902b1
Robin Touche <someone90_6@hotmail.com>
2019-03-15 17:07:52 +0100

    [Level editor] Added view movement via mouse

--- src/level_editor/main.cpp
+++ src/level_editor/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 
 #include "file.h"
+#include "log.h"
 #include "base_entity.h"
 
 int main() {
@@ -17,12 +18,52 @@ int main() {
         }
     }
 
+    float view_pos_x = 500.0;
+    float view_pos_y = 500.0;
+    float view_size = 1000.0;
+
+    int mouse_x, mouse_y;
+
     while (window.isOpen()) {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
             window.close();
         }
 
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::MouseWheelScrolled:
+                    if (event.mouseWheelScroll.delta > 0) {
+                        view_size -= 200;
+                    } else {
+                        view_size += 200;
+                    }
+                    break;
+                case sf::Event::MouseButtonPressed:
+                    if (event.mouseButton.button == sf::Mouse::Button::Right) {
+                        mouse_x = event.mouseButton.x;
+                        mouse_y = event.mouseButton.y;
+                    }
+                default:
+                    break;
+            }
+        }
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Right)) {
+            auto mouse_tmp_pos = sf::Mouse::getPosition(window);
+            view_pos_x += static_cast<float>(mouse_x - mouse_tmp_pos.x);
+            view_pos_y += static_cast<float>(mouse_y - mouse_tmp_pos.y);
+            mouse_x = mouse_tmp_pos.x;
+            mouse_y = mouse_tmp_pos.y;
+        }
+
         window.clear();
+
+        sf::View viewport({view_pos_x, view_pos_y}, {view_size, view_size});
+        window.setView(viewport);
 
         for (auto it = world_objects.begin(); it != world_objects.end(); ++it) {
             (*it)->render(window);
