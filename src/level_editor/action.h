bc5d904522c83102a38354a6ccf9643fb2e763e6
Robin Touche <someone90_6@hotmail.com>
2019-03-26 18:09:01 +0100

    [Level Editor] Added moving objects

---
+++ src/level_editor/action.h
@@ -0,0 +1,4 @@
+enum class Action {
+    NONE,
+    MOVE,
+};
--- src/level_editor/main.cpp
+++ src/level_editor/main.cpp
@@ -3,12 +3,15 @@
 #include "file.h"
 #include "log.h"
 #include "base_entity.h"
+#include "action.h"
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1000,1000), "BLAAAAH");
 
     std::optional<nlohmann::json> j = file::loadJson("assets/world.json");
     std::vector<std::shared_ptr<BaseEntity>> world_objects;
+
+    Action current_action = Action::NONE;
 
     if (j) {
         for (auto it : j.value()) {
@@ -22,7 +25,9 @@ int main() {
     float view_pos_y = 500.0;
     float view_size = 1000.0;
 
-    int mouse_x, mouse_y;
+    sf::Vector2i mouse_pos;
+
+    std::shared_ptr<BaseEntity> current_entity;
 
     while (window.isOpen()) {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
@@ -42,10 +47,34 @@ int main() {
                         view_size += 200;
                     }
                     break;
+                case sf::Event::KeyPressed:
+                    switch (event.key.code) {
+                        case sf::Keyboard::Key::A:
+                            if (current_action == Action::MOVE) {
+                                current_action = Action::NONE;
+                            } else {
+                                current_action = Action::MOVE;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
                 case sf::Event::MouseButtonPressed:
                     if (event.mouseButton.button == sf::Mouse::Button::Right) {
-                        mouse_x = event.mouseButton.x;
-                        mouse_y = event.mouseButton.y;
+                        mouse_pos = sf::Mouse::getPosition(window);
+                    } else if (event.mouseButton.button == sf::Mouse::Button::Left) {
+                        sf::Vector2i pixelPos = sf::Mouse::getPosition(window);
+                        sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos);
+
+                        Hitbox tmp_hbox = {util::Right(worldPos.x), util::Left(worldPos.x), util::Top(worldPos.y), util::Bottom(worldPos.y)};
+                        for (auto it : world_objects) {
+                            if (it->getAbsHitbox().collides(tmp_hbox)) {
+                                current_entity = it;
+                                mouse_pos = sf::Mouse::getPosition(window);
+                                break;
+                            }
+                            current_entity = nullptr;
+                        }
                     }
                 default:
                     break;
@@ -54,10 +83,18 @@ int main() {
 
         if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Right)) {
             auto mouse_tmp_pos = sf::Mouse::getPosition(window);
-            view_pos_x += static_cast<float>(mouse_x - mouse_tmp_pos.x);
-            view_pos_y += static_cast<float>(mouse_y - mouse_tmp_pos.y);
-            mouse_x = mouse_tmp_pos.x;
-            mouse_y = mouse_tmp_pos.y;
+            view_pos_x += static_cast<float>(mouse_pos.x - mouse_tmp_pos.x);
+            view_pos_y += static_cast<float>(mouse_pos.y - mouse_tmp_pos.y);
+            mouse_pos = mouse_tmp_pos;
+        }
+
+        if (current_action == Action::MOVE) {
+            if (current_entity) {
+                auto mouse_tmp_pos = sf::Mouse::getPosition(window);
+                auto current_pos = current_entity->getPosition();
+                current_entity->setPosition(util::X(current_pos.x - (mouse_pos.x - mouse_tmp_pos.x)), util::Y(current_pos.y - (mouse_pos.y - mouse_tmp_pos.y)));
+                mouse_pos = mouse_tmp_pos;
+            }
         }
 
         window.clear();
