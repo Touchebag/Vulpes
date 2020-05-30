16f25aff778ac55676d685585576fb1c3c07f7b1
Robin Touche <someone90_6@hotmail.com>
2020-05-30 17:11:10 +0200

    Made level editor integration optional

---
+++ src/game/game_loop.cpp
@@ -0,0 +1,92 @@
+#include <memory>
+
+#include <json.hpp>
+
+#include "base_entity.h"
+#include "system/world.h"
+#include "utils/common.h"
+#include "utils/log.h"
+#include "system/input_event.h"
+#include "system/render.h"
+
+#define PHYSICS_FRAME_RATE 60
+#define MS_PER_FRAME 1000 / PHYSICS_FRAME_RATE
+
+int game_main(sf::RenderWindow& window, std::string level_file_path) {
+    auto worldInstRead = World::getInstance<World::IWorldRead>();
+    auto worldInstWrite = World::getInstance<World::IWorldModify>();
+
+    sf::Time frames;
+    sf::Clock frame_time;
+
+    worldInstWrite.loadWorldFromFile(level_file_path);
+
+    Input::getInstance().setKeyboardMap(
+            {{sf::Keyboard::Key::Space, Actions::Action::JUMP},
+            {sf::Keyboard::Key::Left, Actions::Action::MOVE_LEFT},
+            {sf::Keyboard::Key::Right, Actions::Action::MOVE_RIGHT},
+            {sf::Keyboard::Key::LShift, Actions::Action::DASH},
+            {sf::Keyboard::Key::Z, Actions::Action::ATTACK}});
+
+    frame_time.restart();
+
+    Render& renderInst = Render::getInstance();
+
+    {
+        auto view_size = window.getView().getSize();
+        renderInst.setView(0, 0, view_size.x, view_size.y);
+    }
+
+    while (window.isOpen()) {
+        frames += frame_time.getElapsedTime();
+        frame_time.restart();
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+            window.close();
+        }
+
+        window.clear();
+
+        // If we have rendered more than one physics frame then advance physics
+        while (frames.asMilliseconds() >= (MS_PER_FRAME)) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                switch (event.type) {
+                    case sf::Event::Closed:
+                        window.close();
+                        break;
+                    case sf::Event::Resized:
+                        renderInst.resizeView(static_cast<float>(event.size.width), static_cast<float>(event.size.height));
+                        break;
+                    case sf::Event::KeyPressed:
+                        if (event.key.code == sf::Keyboard::Key::P) {
+                            return 0;
+                        }
+                        LOGV("Key pressed %i", event.key.code);
+                        Input::getInstance().keyEvent(event.key.code, true);
+                        break;
+                    case sf::Event::KeyReleased:
+                        LOGV("Key released %i", event.key.code);
+                        Input::getInstance().keyEvent(event.key.code, false);
+                        break;
+                    default:
+                        LOGV("Unknown event %i", event.type);
+                        break;
+                }
+            }
+
+            worldInstWrite.update();
+
+            frames -= sf::milliseconds(MS_PER_FRAME);
+        }
+
+        auto view_pos = worldInstRead.getPlayerPosition();
+        renderInst.moveView(static_cast<float>(view_pos.x), static_cast<float>(view_pos.y));
+
+        renderInst.render(window);
+
+        window.display();
+    }
+
+    return 0;
+}
